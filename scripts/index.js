c145df98e9a0e0b26a52285389a7f79c88da12a5
Guryanov David <davidguryanov@yandex.ru>
2022-05-06 00:01:07 +0600

    feat: duplicate, remove, edit

--- scripts/index.js
+++ scripts/index.js
@@ -9,14 +9,53 @@ const initialItems = [
   'Решить задачу на Codewars',
 ];
 
+// document.createElement()
+
 const itemListWrapper = document.querySelector('.items');
 const form = document.querySelector('.add_form');
 const template = document.getElementById('item');
+
+const handleDeleteItem = (evt) => {
+  evt.target.closest('.todo_item').remove();
+};
+
+const handleDuplicateItem = (evt) => {
+  const thisElement = evt.target.closest('.todo_item');
+  const newItem = getItemElement(
+    thisElement.querySelector('.todo_item__header').textContent
+  );
+  itemListWrapper.insertBefore(newItem, thisElement.nextSibling);
+};
+
+const handleEditItem = (evt) => {
+  const title = evt.target
+    .closest('.todo_item')
+    .querySelector('.todo_item__header');
+  const titleText = title.textContent;
+  title.remove();
+  const newInput = document.createElement('input');
+  newInput.value = titleText;
+  newInput.classList.add('todo_item__header-editing');
+  newInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      title.textContent = newInput.value;
+      newInput.remove();
+      evt.target.closest('.todo_item').prepend(title);
+    }
+  });
+  evt.target.closest('.todo_item').prepend(newInput);
+};
 
 const getItemElement = (title) => {
   const newItemElement = template.content.cloneNode(true);
   const newItemTitle = newItemElement.querySelector('.todo_item__header');
   newItemTitle.textContent = title;
+  const deleteButton = newItemElement.querySelector('.button__delete');
+  const editButton = newItemElement.querySelector('.button__edit');
+  const duplicateButton = newItemElement.querySelector('.button__duplicate');
+  deleteButton.addEventListener('click', handleDeleteItem);
+  duplicateButton.addEventListener('click', handleDuplicateItem);
+  editButton.addEventListener('click', handleEditItem);
   return newItemElement;
 };
 
