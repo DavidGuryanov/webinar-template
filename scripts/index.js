3e7fa0cbe9d714ca168891db90661f85b1be74b0
Guryanov David <davidguryanov@yandex.ru>
2022-05-05 23:34:18 +0600

    refactor: initial rendering

--- scripts/index.js
+++ scripts/index.js
@@ -11,22 +11,17 @@ const initialItems = [
 
 const itemListWrapper = document.querySelector('.items');
 const form = document.querySelector('.add_form');
-// insertAdjacentHTML
+const template = document.getElementById('item');
+
+const getItemElement = (title) => {
+  const newItemElement = template.content.cloneNode(true);
+  const newItemTitle = newItemElement.querySelector('.todo_item__header');
+  newItemTitle.textContent = title;
+  return newItemElement;
+};
 
 const renderItem = (wrap, title) => {
-  wrap.insertAdjacentHTML(
-    'beforeend',
-    `
-      <li class="todo_item">
-                  <h3 class="todo_item__header">${title}</h3>
-                  <div class="buttons">
-                      <button type="button" class="button button__edit"></button>
-                      <button type="button" class="button button__duplicate"></button>
-                      <button type="button" class="button button__delete"></button>
-                  </div>
-              </li>
-      `
-  );
+  wrap.append(getItemElement(title));
 };
 
 initialItems.forEach((title) => {
@@ -35,7 +30,9 @@ initialItems.forEach((title) => {
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const title = e.target.title.value;
-  renderItem(itemListWrapper, title);
+  const newTitle = e.target.title.value;
+  if (newTitle) {
+    renderItem(itemListWrapper, newTitle);
+  }
   e.target.title.value = '';
 });
